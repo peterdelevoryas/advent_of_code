bb7bb1c8ed91b7d4c59fa21c9e906bc7d119fb93
Peter Delevoryas <pdel@meta.com>
2022-12-25 18:57:17 -0800

    [2022 day 1 part 2] Finished

--- 2022/day/1/main.c
+++ 2022/day/1/main.c
@@ -3,30 +3,43 @@
 #include <string.h>
 #include <assert.h>
 
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+static void insert(int x, int n, int a[n])
+{
+    for (int i = 0; i < n; i++) {
+        if (x < a[i]) {
+            continue;
+        }
+        memmove(&a[i + 1], &a[i], (n - i - 1) * sizeof(int));
+        a[i] = x;
+        break;
+    }
+}
 
 int main(int argc, char **argv)
 {
     FILE *f;
     char buf[32];
-    int max_calories_seen = -1;
-    int sum_calories = 0;
+    int top[3] = {-1, -1, -1};
+    int sum = 0;
 
     f = fopen("input", "r");
     assert(f);
 
-
     while (fgets(buf, sizeof(buf), f)) {
         if (buf[0] == '\n') {
-            printf("%d %d\n", sum_calories, max_calories_seen);
-            max_calories_seen = max(sum_calories, max_calories_seen);
-            sum_calories = 0;
+            printf("sum=%d, top = [%d, %d, %d]\n", sum, top[0], top[1], top[2]);
+            insert(sum, ARRAY_SIZE(top), top);
+            printf("top = [%d, %d, %d]\n", top[0], top[1], top[2]);
+            sum = 0;
             continue;
         }
-        sum_calories += atoi(buf);
+        sum += atoi(buf);
     }
 
-    printf("%d\n", max_calories_seen);
+    printf("%d\n", top[0] + top[1] + top[2]);
 
     fclose(f);
 }
